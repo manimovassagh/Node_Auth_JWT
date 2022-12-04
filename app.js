2da45e1b87da5a5986290477b2552ef17e41af33
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-04 23:38:34 +0100

    handle error

--- app.js
+++ app.js
@@ -5,15 +5,25 @@ require("dotenv").config();
 
 const app = express();
 
-const obj = {
-  name: "mani",
-  age: "44",
-};
-
 app.get("/", async (req, res, next) => {
-  res.send(obj);
+  res.send("Salam Mani");
 });
 
+app.use(async (req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
 const PORT = process.env.port || 3000;
 
 app.listen(PORT, () => {
