609346b5f88414182ca6aee7b639b9f0e3c6446a
Mani Movassagh <Mani.mgh@gmail.com>
2022-12-04 23:30:13 +0100

    add controller

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const express =require('express')
-const morgan =require('morgan')
-const createError =require('http-errors')
-require('dotenv').config()
+const express = require("express");
+const morgan = require("morgan");
+const createError = require("http-errors");
+require("dotenv").config();
+
+const app = express();
+
+
+
+app.get('/',async(req,res,next)=>{
+    res.send('salam')
+} )
+
+const PORT = process.env.port || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
+});
